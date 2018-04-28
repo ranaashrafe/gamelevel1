700d84c961e3809b0c02a920a050cf47c512c536
Rana-Ashraf <ranaashrafe27@gmail.com>
2018-04-28 17:10:22 +0200

    level1

---
+++ Project p2a/Assets/Scripts/FirePistol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePistol : MonoBehaviour {
+	public GameObject TheGun;
+	public GameObject MuzzleFlash;
+	public AudioSource GunFire;
+	public bool IsFiring = false;
+
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			if (IsFiring == false) {
+				StartCoroutine (FiringPistol ());
+
+			}
+		}
+		
+	}
+	IEnumerator FiringPistol(){
+		IsFiring = true;
+		TheGun.GetComponent<Animation> ().Play("PistolShot");
+		MuzzleFlash.SetActive (true);
+		MuzzleFlash.GetComponent<Animation> ().Play ("MuzzleAnim");
+
+
+		GunFire.Play();
+		yield return new WaitForSeconds (0.5f);
+		IsFiring = false;
+	}
+}
